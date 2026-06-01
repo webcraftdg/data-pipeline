dac5b4ac39243dab114e4fbd60510df9c3f57518
David Ghyse <dghyseredcat.fr>
2026-06-01 15:32:41 +0200

    update options conditions

--- src/io/inputs/XlsInput.php
+++ src/io/inputs/XlsInput.php
@@ -82,8 +82,8 @@ class XlsInput implements InputInterface, InputSpreadsheetInterface, ValidateRul
     public function open(): void
     {
         $filePath = ($this->options['path']) ?? '';
-        if (empty($filePath) === true) {
-            throw new InvalidArgumentException('XlsInput option "path" is invalid.');
+        if (empty($filePath) === true && file_exists($filePath) === false) {
+            throw new InvalidArgumentException('XlsInput option "path" is invalid or not set.');
         }
         $this->spreadsheet = $this->prepareSpreadSheet($filePath);
         if ($this->spreadsheet instanceof Spreadsheet) {
